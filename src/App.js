680e9b0e8b26388a16f28d63b87cf547122b03cd
faparipik <bruncevicfaruk@gmail.com>
2021-02-16 17:16:10 +0100

    redux and router added

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
 /* eslint-disable react/jsx-filename-extension */
 import React from 'react';
+import { Provider } from 'react-redux';
+import { Switch, BrowserRouter } from 'react-router-dom';
+import store from './redux/store';
 
 function App() {
-  return <div>test</div>;
+  return (
+    <Provider store={store}>
+      <div>test</div>
+      <BrowserRouter>
+        <Switch>
+          {/* <Route path="/login" component={Login} /> */}
+          {/* <Route path="/file-viewer" component={FileViewer} /> */}
+          {/* <PrivateRoute path="/sign-in-as" component={SignInAs} /> */}
+          {/* <PrivateRoute path="/" component={Homepage} /> */}
+        </Switch>
+      </BrowserRouter>
+    </Provider>
+  );
 }
 
 export default App;
---
+++ src/modules/test/redux/redux.js
@@ -0,0 +1,8 @@
+export default (state = {}, action) => {
+  switch (action) {
+    case 'nesto':
+      return { ...state };
+    default:
+      return { ...state };
+  }
+};
