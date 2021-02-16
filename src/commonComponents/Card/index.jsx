bb448deb654e93eb17d885c6fe0b3a2940cb1b84
faparipik <bruncevicfaruk@gmail.com>
2021-02-16 21:07:57 +0100

    Card

---
+++ src/commonComponents/Card/index.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import { Form, Button } from 'reactstrap';
+import { useHistory } from 'react-router-dom';
+
+const Card = ({ title, children, onSubmit }) => {
+  const history = useHistory();
+  const submitForm = (e) => {
+    e.preventDefault();
+    onSubmit(e);
+  };
+  return (
+    <div className="container">
+      <div className="container_title">{title}</div>
+      <div className="container_separator" />
+      <div className="container_body">
+        <Form noValidate onSubmit={submitForm}>
+          {children}
+        </Form>
+      </div>
+      <div className="container_separator" />
+      <div className="container_footer">
+        <Button
+          onClick={() => history.goBack()}
+          outline
+          size="lg"
+          color="danger"
+        >
+          Back
+        </Button>
+        <Button
+          outline
+          size="lg"
+          color="danger"
+          type="submit"
+          onClick={submitForm}
+          className="float-right"
+        >
+          Confirm
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+Card.propTypes = {
+  title: PropTypes.string,
+  onSubmit: PropTypes.func,
+  children: PropTypes.string.isRequired,
+};
+
+Card.defaultProps = {
+  title: 'Title',
+  onSubmit: () => {},
+};
+
+export default Card;
