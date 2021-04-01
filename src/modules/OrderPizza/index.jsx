5202ffeed84d05717feda4477d3ce710ffca4f32
faparipik <bruncevicfaruk@gmail.com>
2021-04-01 19:06:57 +0200

    Added order sending function

--- src/modules/OrderPizza/index.jsx
+++ src/modules/OrderPizza/index.jsx
@@ -1,14 +1,19 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
 import SelectPizzaSize from './SelectPizzaSize';
 import SelectIngredients from './SelectIngredients';
+import { sendOrder } from './redux/orderPizzaActions';
 
 import Card from '../../commonComponents/Card/index';
 
-const index = () => (
-  <Card title="Order Your Pizza">
-    <SelectPizzaSize />
-    <SelectIngredients />
-  </Card>
-);
+const index = () => {
+  const dispatch = useDispatch();
+  return (
+    <Card title="Order Your Pizza" onSubmit={() => dispatch(sendOrder())}>
+      <SelectPizzaSize />
+      <SelectIngredients />
+    </Card>
+  );
+};
 
 export default index;
